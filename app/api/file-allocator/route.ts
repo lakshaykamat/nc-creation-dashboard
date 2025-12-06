40e959187ed3c810a784f15141dfb7163ae10bb2
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2025-12-06 11:44:24 +0530

    Fix imports

--- app/api/file-allocator/route.ts
+++ app/api/file-allocator/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
-import { logger } from "@/lib/logger"
+import { logger } from "@/lib/common/logger"
 import {
   getSampleAllocationData,
   shouldUseSampleData,
