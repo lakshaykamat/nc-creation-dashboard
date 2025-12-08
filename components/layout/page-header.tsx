4ddb7e1bf8f7d8c47c75516877198118f0f01d90
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2025-12-08 09:42:53 +0530

    Update

--- components/layout/page-header.tsx
+++ components/layout/page-header.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import { Separator } from "@/components/ui/separator"
+import { SidebarTrigger } from "@/components/ui/sidebar"
+import { useIsMobile } from "@/hooks/common/use-mobile"
 
 interface PageHeaderProps {
   title: string
@@ -6,11 +10,16 @@ interface PageHeaderProps {
 }
 
 export function PageHeader({ title, description }: PageHeaderProps) {
+  const isMobile = useIsMobile()
+
   return (
     <>
-      <div className="space-y-1">
-        <h1 className="text-2xl font-semibold">{title}</h1>
-        <p className="text-sm text-muted-foreground">{description}</p>
+      <div className="flex items-center gap-3">
+        {isMobile && <SidebarTrigger/>}
+        <div className="space-y-1">
+          <h1 className="text-2xl font-semibold">{title}</h1>
+          <p className="text-sm text-muted-foreground">{description}</p>
+        </div>
       </div>
       <Separator />
     </>
--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -273,7 +273,7 @@ function SidebarTrigger({
       }}
       {...props}
     >
-      <PanelLeftIcon />
+      <PanelLeftIcon className="w-16 h-16" />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
